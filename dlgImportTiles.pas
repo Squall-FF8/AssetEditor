15d1b83c90d190aac4ae720085a47e0830f4ca30
Squall-FF8 <35965968+Squall-FF8@users.noreply.github.com>
2024-02-24 15:45:34 +0200

    added Presets functionality

--- dlgImportTiles.pas
+++ dlgImportTiles.pas
@@ -29,9 +29,13 @@ type
     lBytesPerTile: TLabel;
     Label8: TLabel;
     lTotalBytes: TLabel;
+    Label9: TLabel;
+    cbPreset: TComboBox;
     procedure bOKClick(Sender: TObject);
     procedure bCancelClick(Sender: TObject);
     procedure ControlChange(Sender: TObject);
+    procedure FormCreate(Sender: TObject);
+    procedure cbPresetChange(Sender: TObject);
   private
     procedure Calc;
   public
@@ -45,6 +49,30 @@ implementation
 
 uses StdConvs;
 {$R *.dfm}
+
+type
+  tPreset = record
+    Text: string [20]; //used in combpbpx
+    W, H: integer; //tile Width/Height
+    BPP:  integer;
+  end;
+
+const
+  c2bpp      = 0;
+  c4bpp      = 1;
+  c8bpp      = 2;
+  cPresetNum = 9;
+  cPreset: array[0 .. cPresetNum-1] of tPreset = (
+    (Text: 'Tile 8x8 in 8 bpp'; W: 8; H: 8; BPP: c8bpp),
+    (Text: 'Tile 12x12 in 8 bpp'; W: 12; H: 12; BPP: c8bpp),
+    (Text: 'Tile 16x16 in 8 bpp'; W: 16; H: 16; BPP: c8bpp),
+    (Text: 'Tile 8x8 in 4 bpp'; W: 8; H: 8; BPP: c4bpp),
+    (Text: 'Tile 12x12 in 4 bpp'; W: 12; H: 12; BPP: c4bpp),
+    (Text: 'Tile 16x16 in 4 bpp'; W: 16; H: 16; BPP: c4bpp),
+    (Text: 'Tile 8x8 in 2 bpp'; W: 8; H: 8; BPP: c2bpp),
+    (Text: 'Tile 12x12 in 2 bpp'; W: 12; H: 12; BPP: c2bpp),
+    (Text: 'Tile 16x16 in 2 bpp'; W: 16; H: 16; BPP: c2bpp)
+  );
 
 
 procedure TdlgImportTile.bOKClick(Sender: TObject);
@@ -64,9 +92,9 @@ begin
   lNum.Caption := 'max ' + IntToStr(seTileNum.MaxValue);
 
   n := seTileW.Value * seTileH.Value * (2 shl cbTileBPP.ItemIndex) div 8;
-  lBytesPerTile.Caption := format('%.d (%.2x)', [n, n]);
+  lBytesPerTile.Caption := format('%.d  ($%.2x)', [n, n]);
   n := n * seTileNum.Value;
-  lTotalBytes.Caption   := format('%.d (%.2x)', [n, n]);
+  lTotalBytes.Caption   := format('%.d  ($%.2x)', [n, n]);
 end;
 
 
@@ -95,6 +123,27 @@ end;
 procedure TdlgImportTile.ControlChange(Sender: TObject);
 begin
   Calc;
+end;
+
+procedure TdlgImportTile.FormCreate(Sender: TObject);
+  var i: integer;
+begin
+  for i := 0 to cPresetNum -1 do
+    cbPreset.Items.Add(cPreset[i].Text);
+  cbPreset.ItemIndex := 2;
+end;
+
+procedure TdlgImportTile.cbPresetChange(Sender: TObject);
+  var ind: integer;
+begin
+  ind := cbPreset.ItemIndex;
+  if ind = -1 then exit;
+
+  seTileH.Value := cPreset[ind].H;
+  seTileW.Value := cPreset[ind].W;
+  cbTileBPP.ItemIndex := cPreset[ind].BPP;
+  Calc;
+  seTileNum.Value := seTileNum.MaxValue;
 end;
 
 end.
